dbbf4881a8e99ecca419365395dd4f672cabb35b
DenverJensen <jensendenver@gmail.com>
2021-10-16 20:05:41 -0600

    navigation complete. User class started

---
+++ User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment5
+{
+    class User
+    {
+        private string name;
+
+        private int age;
+
+        private int[] scores;
+
+        private int run_time;
+
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public int[] Scores { get => scores; set => scores = value; }
+        public int Run_time { get => run_time; set => run_time = value; }
+    }
+}
